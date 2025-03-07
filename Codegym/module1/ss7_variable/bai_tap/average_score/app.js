c9fe157fcdc9f5cb574e68f94dcca39e9ca07d45
1Ir-is <mhuynk1005@gmail.com>
2025-03-07 14:35:14 +0700

    feat: style and validate

--- Codegym/module1/ss7_variable/bai_tap/average_score/app.js
+++ Codegym/module1/ss7_variable/bai_tap/average_score/app.js
@@ -2,10 +2,20 @@ function calculateAverage() {
   let physicScore = parseFloat(document.getElementById("physic").value);
   let chemistryScore = parseFloat(document.getElementById("chemistry").value);
   let biologyScore = parseFloat(document.getElementById("biology").value);
-
+  if (
+    isNaN(chemistryScore) ||
+    isNaN(physicScore) ||
+    isNaN(biologyScore) ||
+    physicScore <= 0 ||
+    chemistryScore <= 0 ||
+    biologyScore <= 0
+  ) {
+    alert("Vui long nhap so lieu hop le");
+    return;
+  }
   let sumScore = physicScore + chemistryScore + biologyScore;
-  let averageScore = sumScore / 3;
+  let averageScore = parseInt(sumScore / 3);
 
   document.getElementById("result").innerHTML =
-    "Diem tong: " + sumScore + "<br>Diem trung binh: " + averageScore;
+    "Diem tong: " + sumScore + "<br><br>Diem trung binh: " + averageScore;
 }
