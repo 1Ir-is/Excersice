5250df21b04473c6f6e17705193e4f79589d28b5
1Ir-is <mhuynk1005@gmail.com>
2025-03-07 08:36:56 +0700

    feat: create js basic

---
+++ Codegym/module1/ss6_javascript/thuc_hanh/app.js
@@ -0,0 +1,27 @@
+/*
+    Bai 1
+*/
+function showMessageForLess1() {
+  alert("Hello world!");
+}
+
+/*
+    Bai 2
+*/
+function showMessageForLess2() {
+  document.getElementById("messageDiv").innerHTML = "Hello worlds";
+}
+
+/*
+    Bai 3
+*/
+function showMessageForLess3() {
+  document.writeln("Hello");
+}
+
+/*
+    Bai 4
+*/
+function showMessageForLess4() {
+  console.log("Hello");
+}
