5398dbc145cebba2cf9a5e011c1bbd2661e886f1
1Ir-is <mhuynk1005@gmail.com>
2025-03-11 14:31:15 +0700

    create find max in 3 nums

---
+++ Codegym/module1/ss8_condition_statement/bai_tap/check_max_num_3_nums/app.js
@@ -0,0 +1,16 @@
+function findMax() {
+  const num1 = parseInt(document.getElementById("num1").value);
+  const num2 = parseInt(document.getElementById("num2").value);
+  const num3 = parseInt(document.getElementById("num3").value);
+  let max;
+
+  if (num1 >= num2 && num1 >= num3) {
+    max = num1;
+  } else if (num2 >= num1 && num2 >= num3) {
+    max = num2;
+  } else {
+    max = num3;
+  }
+
+  document.getElementById("result").innerText = `The largest number is ${max}.`;
+}
