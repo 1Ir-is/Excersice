219801b004b05cbb1cc0d2bf63681d7d78a0e170
1Ir-is <mhuynk1005@gmail.com>
2025-04-04 11:13:10 +0700

    done thuc hanh 1

---
+++ Codegym/module2/module_2/src/ss1_tong_quan_java/thuc_hanh/thuc_hanh_1/SystemTime.java
@@ -0,0 +1,11 @@
+package ss1_tong_quan_java.thuc_hanh.thuc_hanh_1;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Hệ thống hiện tại là: " + now);
+    }
+}
