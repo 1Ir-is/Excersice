89727044eff3e6d95edb264fea7321acac1d0c70
1Ir-is <mhuynk1005@gmail.com>
2025-04-15 17:36:34 +0700

    case study - model - create class booking and contract

--- Codegym/module2/module_2/src/bai_tap_them/case_study_furuma/model/Booking.java
+++ Codegym/module2/module_2/src/bai_tap_them/case_study_furuma/model/Booking.java
@@ -10,6 +10,59 @@ public class Booking {
     private String facilityId;
 
     public Booking(String bookingId, String bookingDate, String startDate, String endDate, String customerId, String facilityId) {
+        this.bookingId = bookingId;
+        this.bookingDate = bookingDate;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.customerId = customerId;
+        this.facilityId = facilityId;
+    }
 
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFacilityId() {
+        return facilityId;
+    }
+
+    public void setFacilityId(String facilityId) {
+        this.facilityId = facilityId;
     }
 }
