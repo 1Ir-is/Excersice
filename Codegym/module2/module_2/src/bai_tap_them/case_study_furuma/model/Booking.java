90ea5e1d6b8a36ee63e1d49634259b356ed72f51
1Ir-is <mhuynk1005@gmail.com>
2025-04-15 17:30:48 +0700

    case study - model - add Booking class

---
+++ Codegym/module2/module_2/src/bai_tap_them/case_study_furuma/model/Booking.java
@@ -0,0 +1,15 @@
+package bai_tap_them.case_study_furuma.model;
+
+public class Booking {
+    private String bookingId;
+    private String bookingDate;
+    private String startDate;
+    private String endDate;
+
+    private String customerId;
+    private String facilityId;
+
+    public Booking(String bookingId, String bookingDate, String startDate, String endDate, String customerId, String facilityId) {
+
+    }
+}
