ff415512e732a053a4036700e4af224aa214998f
1Ir-is <mhuynk1005@gmail.com>
2025-04-15 17:24:59 +0700

    case study - model - create Person, Customer and Employee class

---
+++ Codegym/module2/module_2/src/bai_tap_them/case_study_furuma/model/Customer.java
@@ -0,0 +1,35 @@
+package bai_tap_them.case_study_furuma.model;
+
+public class Customer extends Person {
+
+    private String customerType;
+    private String address;
+
+    public Customer(String id, String name, String dateOfBirth, String gender, String idCard, String phoneNuber, String email,
+                    String customerType, String address) {
+        super(id, name, dateOfBirth, gender, idCard, phoneNuber, email);
+        this.customerType = customerType;
+        this.address = address;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Customer: " + getName() + ", Customer Type: " + customerType + ", Address: " + address;
+    }
+}
---
+++ Codegym/module2/module_2/src/bai_tap_them/case_study_furuma/model/Employee.java
@@ -0,0 +1,44 @@
+package bai_tap_them.case_study_furuma.model;
+
+public class Employee extends Person {
+    private String qualification;
+    private String position;
+    private double salary;
+
+    public Employee(String id, String name, String dateOfBirth, String gender, String idCard, String phoneNuber, String email,
+                    String qualification, String position, double salary) {
+        super(id, name, dateOfBirth, gender, idCard, phoneNuber, email);
+        this.qualification = qualification;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Employee: " + getName() + ", Position: " + position + ", Qualification: " + qualification + ", Salary: " + salary;
+    }
+}
