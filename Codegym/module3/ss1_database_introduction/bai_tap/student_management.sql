901122423f7f1b5cd6489e9d4e59d7887135af80
1Ir-is <mhuynk1005@gmail.com>
2025-05-19 11:01:34 +0700

    them module 3 va hoan thanh bai tap

---
+++ Codegym/module3/ss1_database_introduction/bai_tap/student_management.sql
@@ -0,0 +1,34 @@
+create database student_management;
+
+use student_management;
+
+create table Class (
+	id int,
+    name varchar(255),
+    primary key(id)
+);
+
+create table Teacher (
+	id int,
+    name varchar(255),
+    age int,
+    country varchar(255),
+    primary key(id)
+);
+
+select * from Class;
+
+insert into Class (id, name) values 
+(1,'10A1'),
+(2,'10A2'),
+(3,'11B1'),
+(4,'12C1');
+
+
+select * from Teacher;
+
+insert into Teacher (id, name, age, country) values 
+(1,'Nguyen Van An', 35, 'Vietnam'),
+(2,'Tran Thi Binh', 42, 'Vietnam'),
+(3,'John Smith', 38, 'USA'),
+(4,'Nguyen Thi Lan', 29, 'Vietnam');
